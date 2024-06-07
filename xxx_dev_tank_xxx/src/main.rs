0e2f6cc70bd83d3b8ecea333757cf089ca0dfd44
trifoil <unixsslayer@gmail.com>
2024-06-07 23:33:33 +0200

    refs wohooo

--- xxx_dev_tank_xxx/src/main.rs
+++ xxx_dev_tank_xxx/src/main.rs
@@ -2,6 +2,11 @@ mod subroutines;
 
 fn main() {
     subroutines::greet();
-    let zero_turn = subroutines::wheel_2_zero_turn();
-    println!("{:?}", zero_turn);
+    
+    let zero_turn_tuple = subroutines::wheel_2_zero_turn_tuple();
+    println!("{:?}", zero_turn_tuple);
+
+    let mut var_test = 0;
+    subroutines::wheel_2_zero_turn_reference(&mut var_test,5);
+    println!("{}", var_test);
 }
--- xxx_dev_tank_xxx/src/subroutines.rs
+++ xxx_dev_tank_xxx/src/subroutines.rs
@@ -2,9 +2,13 @@ pub fn greet() {
     println!("EUROBEAT!!!");
 }
 
-pub fn wheel_2_zero_turn() -> (i32,i32) {
+pub fn wheel_2_zero_turn_tuple() -> (i32,i32) {
     println!("wheel_2_zero_turn");
     let valeur_a = 2;
     let valeur_b = 3;
-    (valeur_a, valeur_b)
+    (valeur_a, valeur_b) //NO SEMICOLON!!!
+}
+
+pub fn wheel_2_zero_turn_reference(var: &mut i32, new_value: i32) {
+    *var = new_value;
 }
