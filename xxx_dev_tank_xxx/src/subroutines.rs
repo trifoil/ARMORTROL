c4872929b3ac11f593b49a321d2e4806ba43d66d
trifoil <unixsslayer@gmail.com>
2024-06-08 03:35:04 +0200

    new fn

--- xxx_dev_tank_xxx/src/subroutines.rs
+++ xxx_dev_tank_xxx/src/subroutines.rs
@@ -6,6 +6,10 @@ pub fn wheel_input_correction(wheel_input: f32, wheel_input_corrected: &mut f32)
     *wheel_input_corrected = wheel_input * 2.0 -255.0;
 }
 
+pub fn acc_brake_direction_2_throttle() {
+    
+}
+
 pub fn wheel_2_zero_turn_reference(wheel_input_corrected: f32, throttle: f32, left_track_motor: &mut f32,  right_track_motor: &mut f32) {
     *left_track_motor = wheel_input_corrected;
     *right_track_motor = throttle;
