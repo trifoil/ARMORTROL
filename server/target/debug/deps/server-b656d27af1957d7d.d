d54227310bed7c2e7b43624624793b2877385502
trifoil <unixsslayer@gmail.com>
2024-06-21 03:35:58 +0200

    README UPDATED

---
+++ server/target/debug/deps/server-b656d27af1957d7d.d
@@ -0,0 +1,5 @@
+/home/x/Documents/ARMORTROL/server/target/debug/deps/libserver-b656d27af1957d7d.rmeta: src/main.rs
+
+/home/x/Documents/ARMORTROL/server/target/debug/deps/server-b656d27af1957d7d.d: src/main.rs
+
+src/main.rs:
---
+++ vehicle/target/debug/deps/vehicle-08370a431ed3a218.d
@@ -0,0 +1,5 @@
+/home/x/Documents/ARMORTROL/vehicle/target/debug/deps/libvehicle-08370a431ed3a218.rmeta: src/main.rs
+
+/home/x/Documents/ARMORTROL/vehicle/target/debug/deps/vehicle-08370a431ed3a218.d: src/main.rs
+
+src/main.rs:
