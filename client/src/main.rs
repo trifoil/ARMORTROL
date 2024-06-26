6593433b7b817dafba17a75d7ae231e464288aff
trifoil <unixsslayer@gmail.com>
2024-06-26 18:54:53 +0200

    unit test on subroutines

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,15 @@
 mod input;
+mod subroutines;
 
 fn main() {
-    input::input_test();
+    let thrl:f32 = 1.0;
+    let wheel:f32 = 0.75;
+    let mut left:f32 = 0.0;
+    let mut right:f32 =0.0;
+
+    //input::input_test();
+    subroutines::input_2_tank(thrl, wheel, 
+        &mut left, &mut right);
+    
+    println!("{},{}", left, right);
 }
