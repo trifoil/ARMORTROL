3ffb3dcf2000f5fee2828a148f143a91a243004e
KoruJuriSan <mr.collejoulian@gmail.com>
2024-06-26 16:58:06 +0200

    ci: clearing a bit

--- client/target/debug/deps/client-a3c8c2518b223a7c.d
+++
@@ -1,5 +0,0 @@
-/home/x/Documents/ARMORTROL/client/target/debug/deps/libclient-a3c8c2518b223a7c.rmeta: src/main.rs
-
-/home/x/Documents/ARMORTROL/client/target/debug/deps/client-a3c8c2518b223a7c.d: src/main.rs
-
-src/main.rs:
--- vehicle/target/debug/deps/vehicle-a67fc8536b903738.d
+++
@@ -1,5 +0,0 @@
-/home/x/Documents/ARMORTROL/vehicle/target/debug/deps/libvehicle-a67fc8536b903738.rmeta: src/main.rs
-
-/home/x/Documents/ARMORTROL/vehicle/target/debug/deps/vehicle-a67fc8536b903738.d: src/main.rs
-
-src/main.rs:
