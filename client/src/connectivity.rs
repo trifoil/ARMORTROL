c130ec6d935320f7b7d9ef64bc0c2abf803c4a75
KoruJuriSan <mr.collejoulian@gmail.com>
2024-06-27 13:03:42 +0200

    feat: adding a basic input system
    - and removing the old input.rs template.

---
+++ client/src/connectivity.rs
@@ -0,0 +1,39 @@
+use gilrs::{Gilrs, Event, EventType, Axis, Button};
+
+pub fn setup() -> Gilrs {
+    let input_system: Gilrs = Gilrs::new().unwrap();
+    return input_system;
+}
+
+pub fn update(
+    mut input_system: Gilrs,
+    accelerator: &mut f32,
+    stearing_wheels: &mut f32,
+    is_using_break: &mut bool,
+    is_going_forward: &mut bool
+) -> Gilrs {
+
+    while let Some(Event { id, event, time }) = input_system.next_event() {
+        match event  {
+            EventType::AxisChanged(axis, value, _) => {
+                    if matches!(axis, Axis::LeftStickX) {
+                        *stearing_wheels = value;
+                    }
+            },
+            EventType::ButtonPressed(button, _) => {
+                if matches!(button, Button::East) {
+                    *is_going_forward = !*is_going_forward
+                }
+            },
+            EventType::ButtonChanged(button, value, _) => {
+                if matches!(button, Button::LeftTrigger2) {
+                    *is_using_break = value >= 0.20;
+                } else if matches!(button, Button::RightTrigger2) {
+                    *accelerator = value;
+                }
+            },
+            _ => {}
+        }
+    }
+    return input_system
+}
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,7 +1,17 @@
-mod input;
+mod connectivity;
 mod subroutines;
 
 fn main() {
+
+    // inputs variables
+    let mut accelerator: f32 = 0.0;
+    let mut stearing_wheels: f32 = 0.0;
+    let mut is_using_break: bool = false;
+    let mut is_going_forward: bool = false;
+
+    // input system, used in the loop
+    let mut input_system = connectivity::setup();
+
     let thrl:f32 = 1.0;
     let wheel:f32 = 0.75;
     let mut left:f32 = 0.0;
@@ -13,5 +23,20 @@ fn main() {
         &mut left, &mut right);
 
     println!("{},{}", left, right);
+
+    
+
+    loop {
+        // update inputs values
+        input_system = connectivity::update(
+            input_system,
+            &mut accelerator,
+            &mut stearing_wheels,
+            &mut is_using_break,
+            &mut is_going_forward
+        );
+        // Test for trifoil:
+        // println!("accelerator: {}, stearing_wheels: {}, is_using_break: {}, is_going_forward: {}", accelerator, stearing_wheels, is_using_break, is_going_forward);
+    }
 }
 
