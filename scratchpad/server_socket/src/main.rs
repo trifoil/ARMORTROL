1a018131dc3fbb73acb22f7d00ca6f35884349f5
trifoil <unixsslayer@gmail.com>
2024-07-08 23:45:59 +0200

    client server unit test

--- scratchpad/server_socket/src/main.rs
+++ scratchpad/server_socket/src/main.rs
@@ -1,3 +1,39 @@
+use std::io::{Read, stdout, Write};
+use std::net::TcpListener;
+
+pub fn run(bind_host: &String, port: &u16) -> Result<(), String> {
+    let addr = format!("{}:{}", bind_host, port);
+    let listener = TcpListener::bind(addr.clone()).map_err(|_| format!("Failed to bind to {}", addr))?;
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut s) => {
+                println!("Connection accepted");
+
+                let mut buf = [0; 128];
+                let mut read_bytes = 0;
+                while read_bytes == 0 {
+                    read_bytes = s.read(&mut buf).map_err(|_| "failed to read from socket")?;
+                    println!("received bytes {}", read_bytes);
+                }
+                stdout().write(&buf[0..read_bytes]).map_err(|_| "failed to write to stdout")?;
+                stdout().flush().unwrap();
+            }
+            Err(e) => {
+                println!("Error while accepting incoming connection - {}", e);
+            }
+        }
+    }
+
+    Ok(())
+}
+
 fn main() {
     println!("Hello, world!");
+    let mut host= String::new();
+    host = "192.168.1.160".to_string();
+    let port:u16 = 8069;
+    run(&host, &port);
 }
+
+
