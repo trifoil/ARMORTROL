8dab1f0b75efba4a602638b19b2a3a7d866276e1
trifoil <unixsslayer@gmail.com>
2024-07-09 12:32:29 +0200

    sending bytes, but problem if the client closes before writing in the connected socket

--- scratchpad/client_socket/src/main.rs
+++ scratchpad/client_socket/src/main.rs
@@ -3,11 +3,18 @@ use std::net::TcpStream;
 
 pub fn run(host: &String, port: &u16) -> Result<(), String> {
     let addr = format!("{}:{}", host, port);
-    let mut client = TcpStream::connect(addr.as_str()).map_err(|_| format!("failed to connect to {}", addr))?;
+    //let mut client = TcpStream::connect(addr.as_str()).map_err(|_| format!("failed to connect to {}", addr))?;
 
     //client.write("hello, TCP".as_bytes()).map_err(|_| format!("failed to send"))?;
-    loop{
+    
+    let mut i = 0;
+    let mut client = TcpStream::connect(addr.as_str()).map_err(|_| format!("failed to connect to {}", addr))?;
+    client.write("hello, TCP\n".as_bytes()).map_err(|_| format!("failed to send"))?;
+
+    while i < 3 {
+        let mut client = TcpStream::connect(addr.as_str()).map_err(|_| format!("failed to connect to {}", addr))?;
         client.write("hello, TCP\n".as_bytes()).map_err(|_| format!("failed to send"))?;
+        i+=1;
     }
     Ok(())
 }
