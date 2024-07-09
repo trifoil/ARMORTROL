67ec537d5f3945da33d4375ba0c8af7b02307aae
KoruJuriSan <mr.collejoulian@gmail.com>
2024-07-09 09:07:25 +0200

    perf: improving the input_2_tank function
    - Remplacing if statements with two math functions.
    
    - L(s,a) = (s + 1.0 - |s|) * a
    - R(s,a) = (s + 1.0 - |s|) * a
    - s -> R [-1;1]
    - a -> R [0;1]

--- client/src/subroutines.rs
+++ client/src/subroutines.rs
@@ -1,16 +1,7 @@
-pub fn input_2_tank(throttle_input:f32,wheel_input:f32, 
+pub fn input_2_tank(accelerator: f32,steering_wheels: f32, 
     left_output: &mut f32, right_output: &mut f32){
-        
-    if wheel_input == 0.0 {
-        *left_output = throttle_input;
-        *right_output = throttle_input;
-    } else if  wheel_input < 0.0 {
-        *left_output = throttle_input * (2.0 * wheel_input + 1.0);
-        *right_output = throttle_input;
-    } else {
-        *left_output = throttle_input;
-        *right_output = throttle_input * (-2.0 * wheel_input + 1.0);
-    }
+    *left_output = (steering_wheels + 1.0 - steering_wheels.abs()) * accelerator;
+    *right_output = (-steering_wheels + 1.0 - steering_wheels.abs()) * accelerator;
 }
 
 // pub fn unit_2_arduino(input:f32, output: &mut i32){
