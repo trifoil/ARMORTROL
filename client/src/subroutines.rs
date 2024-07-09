4069e8d7b9a5c5d061ff62aead107ae1b88874b4
trifoil <unixsslayer@gmail.com>
2024-07-09 11:31:22 +0200

    old code as a comment for subroutines

--- client/src/subroutines.rs
+++ client/src/subroutines.rs
@@ -9,3 +9,18 @@ pub fn input_2_tank(accelerator: f32,steering_wheels: f32,
 //     let temp = temp as i32;
 //     *output = temp;
 // }
+
+// pub fn input_2_tank(throttle_input:f32,wheel_input:f32, 
+//     left_output: &mut f32, right_output: &mut f32){
+        
+//     if wheel_input == 0.0 {
+//         *left_output = throttle_input;
+//         *right_output = throttle_input;
+//     } else if  wheel_input < 0.0 {
+//         *left_output = throttle_input * (2.0 * wheel_input + 1.0);
+//         *right_output = throttle_input;
+//     } else {
+//         *left_output = throttle_input;
+//         *right_output = throttle_input * (-2.0 * wheel_input + 1.0);
+//     }
+// }
